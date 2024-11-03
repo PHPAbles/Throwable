aab886dc76e2707567e758d5a4cdf3ba0d1bd2bb
Jay Potter <j@ypotter.ca>
2024-11-03 18:44:32 -0500

    Create ErrorExceptionInterface.php

---
+++ src/PHPAbles/Throwable/ErrorException/ErrorExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPAbles\Throwable\ErrorException;
+
+use PHPAbles\Throwable\ThrowableInterface;
+
+interface ErrorExceptionInterface extends ThrowableInterface
+{ }
