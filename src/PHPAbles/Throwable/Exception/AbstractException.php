c852af2f13d1b818383f58645f77e41f8c2d6215
Jay Potter <j@ypotter.ca>
2024-11-03 18:44:37 -0500

    Create AbstractException.php

---
+++ src/PHPAbles/Throwable/Exception/AbstractException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPAbles\Throwable\Exception;
+
+use \Exception as BaseException;
+
+abstract class AbstractException extends BaseException implements ExceptionInterface
+{ }
