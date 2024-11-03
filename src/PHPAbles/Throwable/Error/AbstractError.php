0f112d338aa338c7951038640b4969aa7ddc0caa
Jay Potter <j@ypotter.ca>
2024-11-03 18:44:42 -0500

    Create AbstractError.php

---
+++ src/PHPAbles/Throwable/Error/AbstractError.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPAbles\Throwable\Error;
+
+use \Error as BaseError;
+
+abstract class AbstractError extends BaseError implements ErrorInterface
+{ }
